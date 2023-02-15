a1ecd36b7fee6307e7744464ee9714b424534d2c
arbelton <zorynnlove@gmail.com>
2023-02-15 08:48:59 -0500

    added ids to class of cities/ added single js to atl

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+let atlantaElement = document.querySelector("#atlanta");
+let atlantaDateElement = atlantaElement.querySelector(".date");
+let atlantaTimeElement = atlantaElement.querySelector(".time");
+let atlantaTime = moment().tz("America/New_York");
+atlantaDateElement.innerHTML = atlantaTime.format("MMMM Do YYYY");
+atlantaTimeElement.innerHTML = atlantaTime.formart(
+  "h:mm:ss[<small>]A[</small>]"
+);
