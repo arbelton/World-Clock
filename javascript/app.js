73e7f1df3421b619f80c0bc1af10fcf16876605a
arbelton <zorynnlove@gmail.com>
2023-02-15 08:53:09 -0500

    removed typo

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,8 @@ let atlantaElement = document.querySelector("#atlanta");
 let atlantaDateElement = atlantaElement.querySelector(".date");
 let atlantaTimeElement = atlantaElement.querySelector(".time");
 let atlantaTime = moment().tz("America/New_York");
+
 atlantaDateElement.innerHTML = atlantaTime.format("MMMM Do YYYY");
-atlantaTimeElement.innerHTML = atlantaTime.formart(
+atlantaTimeElement.innerHTML = atlantaTime.format(
   "h:mm:ss[<small>]A[</small>]"
 );
