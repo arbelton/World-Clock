07d142089992df96ddd46df7104b06d9a4b3c941
arbelton <zorynnlove@gmail.com>
2023-02-17 15:38:43 -0500

    update js

--- javascript/app.js
+++ javascript/app.js
@@ -31,14 +31,16 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = ` <div class="city">
+  citiesElement.innerHTML = `
+   <div class="city">
           <div>
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
             <div class="time">${cityTime.format(
               "h:mm:ss"
             )}<small>${cityTime.format("A")}</small></div></div>
-        </div>`;
+        `;
 }
 
 updateTime();
